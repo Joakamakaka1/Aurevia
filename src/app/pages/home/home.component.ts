ac9523e50cbe7a3a96384e77871ee23f2a45a8c8
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2025-10-03 13:25:11 +0200

    Home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedIonicModule } from 'src/app/shared-ionic.module';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [CommonModule, SharedIonicModule],
+  imports: [CommonModule, SharedIonicModule, RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
