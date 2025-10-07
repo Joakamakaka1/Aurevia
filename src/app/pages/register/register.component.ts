a2be3f2ff6c7f4ae0b7eb4f3fc6c783d91976a86
AngelJS <angel.jimenez2@estudiant.uib.cat>
2025-10-07 13:08:18 +0200

    Feat: Added register form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RegisterFormComponent } from 'src/app/components/register-form/register-form.component';
+import { SharedIonicModule } from 'src/app/shared-ionic.module';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
+    imports: [RegisterFormComponent, SharedIonicModule, CommonModule],
 })
 export class RegisterComponent  implements OnInit {
 
