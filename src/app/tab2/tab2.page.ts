471bd80db931f8afd4851edb52cb25ba4037b157
AngelJS <angel.jimenez2@estudiant.uib.cat>
2025-12-16 14:23:20 +0100

    feat: Implement new Tab 2 featuring a trip creation form with dynamic country selection.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+import { api } from 'src/lib/api';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonContent, CommonModule]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  countries: any[] = [];
 
-  constructor() {}
+  constructor() { }
 
+  fetchCountries = async () => {
+    try {
+      const response = await api.get('country/')
+      console.log(response.data)
+      this.countries = response.data;
+    } catch (error) {
+      console.error('Error fetching countries:', error);
+    }
+  }
+
+  ngOnInit(): void {
+    this.fetchCountries()
+  }
 }
